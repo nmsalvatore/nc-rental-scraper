4b07eeece17e81e166860fd224d21536246db428
Nicholas Salvatore <nmsalvatore@gmail.com>
2022-08-11 07:57:39 -0700

    Refactored paullaw.py and added city to listing_data

--- paullaw.py
+++ paullaw.py
@@ -3,43 +3,54 @@ import requests
 from bs4 import BeautifulSoup
 from utils import get_int
 
-def extract_paul_law_data():
-    res = requests.get('https://paullawpropertymanagement.managebuilding.com/Resident/public/rentals')
+def get_paul_law_data():
+    root = 'https://paullawpropertymanagement.managebuilding.com/'
+    res = requests.get(root + '/Resident/public/rentals')
     soup = BeautifulSoup(res.text, 'lxml')
 
     rentals = []
-    rentals_html = soup.find_all(class_='featured-listing')
 
-    for listing in rentals_html:
+    listings = soup.find_all(class_='featured-listing')
+    for listing in listings:
+        # get url
+        url = root + listing.get('href')
+
+        # get title
+        title = listing.find(class_='featured-listing__title').getText()
+
+        # get city
+        full_address = listing.find(class_='featured-listing__address').getText()
+        city_regex = re.compile(r'^(.*),')
+        city = city_regex.search(full_address).group(1)
+
+        # get price
+        price_str = listing.find(class_='featured-listing__price').getText()
+        price = get_int(price_str)
+
+        # render listing detail view
+        detail_view = requests.get(url)
+        detail_html = BeautifulSoup(detail_view.text, 'lxml')
+
+        # get bed, bath and square footage
+        unit_info = detail_html.find_all(class_='unit-detail__unit-info-item')
+        for item in unit_info:
+            if re.compile(r'.*Bed.*').search(item.getText()):
+                bedrooms = get_int(item.getText())
+            if re.compile(r'.*Bath.*').search(item.getText()):
+                bathrooms = get_int(item.getText())
+            if re.compile(r'.*sqft.*').search(item.getText()):
+                sqft = get_int(item.getText())
+
         listing_data = {
-            'title': listing.find(class_='featured-listing__title').getText(),
-            'price': get_int(listing.find(class_='featured-listing__price').getText()),
-            'url': 'https://paullawpropertymanagement.managebuilding.com' + listing.get('href')
+            'title': title,
+            'city': city,
+            'price': price,
+            'bedrooms': bedrooms,
+            'bathrooms': bathrooms,
+            'sqft': sqft,
+            'url': url,
         }
 
-        listing_url = listing_data.get('url')
-        listing_detail_page = requests.get(listing_url)
-        listing_detail_html = BeautifulSoup(listing_detail_page.text, 'lxml')
-        listing_description = listing_detail_html.find_all(class_='unit-detail__description')
-        listing_data['description'] = listing_description[0].getText() or listing_description[1].getText()
-
-        # Find bed, bath and square footage data
-        unit_info_items = listing_detail_html.find_all(class_='unit-detail__unit-info-item')
-        bed_regex = re.compile(r'.*Bed.*')
-        bath_regex = re.compile(r'.*Bath.*')
-        sqft_regex = re.compile(r'.*sqft.*')
-
-        for item in unit_info_items:
-            if bed_regex.search(item.getText()):
-                bedroom_num = get_int(item.getText())
-                listing_data['bedrooms'] = bedroom_num
-            if bath_regex.search(item.getText()):
-                bathroom_num = get_int(item.getText())
-                listing_data['bathrooms'] = bathroom_num
-            if sqft_regex.search(item.getText()):
-                sqft_num = get_int(item.getText())
-                listing_data['sqft'] = sqft_num
-        
         rentals.append(listing_data)
 
     return rentals
