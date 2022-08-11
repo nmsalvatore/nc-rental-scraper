7c6742db12d8f9940c950f3bf0c638761e43c244
Nicholas Salvatore <nmsalvatore@gmail.com>
2022-08-11 08:09:30 -0700

    Added number of listings to success message

--- scraper.py
+++ scraper.py
@@ -5,5 +5,7 @@ print('\nRetrieving all available rental data...\n')
 
 paul_law_data = get_paul_law_data()
 collins_data = get_collins_data()
+rentals = paul_law_data + collins_data
+rental_num = len(rentals)
 
-print('Rental data retrieved successfully.\n')
+print(f'{rental_num} rentals retrieved successfully.\n')
