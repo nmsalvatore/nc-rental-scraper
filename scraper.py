a5ff03b5b9d4950a9c26a34bf505ac8543ca321f
Nicholas Salvatore <nmsalvatore@gmail.com>
2022-08-18 05:45:41 -0700

    Added pam, acm, grassroots data and selenium dependency to scraper.py

--- scraper.py
+++ scraper.py
@@ -1,11 +1,23 @@
 from paullaw import get_paul_law_data
 from collins import get_collins_data
+from pam import get_pam_data
+from acm import get_acm_data
+from grassroots import get_grass_roots_data
+from selenium import webdriver
 
 print('\nRetrieving all available rental data...\n')
 
 paul_law_data = get_paul_law_data()
 collins_data = get_collins_data()
-rentals = paul_law_data + collins_data
-rental_num = len(rentals)
 
-print(f'{rental_num} rentals retrieved successfully.\n')
+# get data from websites requiring selenium
+driver = webdriver.Firefox()
+pam_data = get_pam_data(driver)
+acm_data = get_acm_data(driver)
+grass_roots_data = get_grass_roots_data(driver)
+driver.close()
+
+# rentals = paul_law_data + collins_data + pam_data
+rentals = paul_law_data + collins_data + pam_data + acm_data + grass_roots_data
+rental_num = len(rentals)
+print(f'\n{rental_num} rentals retrieved successfully.\n')
