490f0876c49b7f68a74bf68521d11457f5325b5c
Nicholas Salvatore <nmsalvatore@gmail.com>
2022-08-20 08:02:49 -0700

    Added check for duplicate database insertions

--- update_db.py
+++ update_db.py
@@ -12,33 +12,48 @@ conn = psycopg2.connect(
     dbname=os.environ['DB_NAME'],
     user=os.environ['DB_USER'],
 )
-
 cur = conn.cursor()
 
 rentals = get_all_rentals()
+new_listing_count = 0
+
 for rental in rentals:
-    cur.execute("""
-        INSERT INTO nc_rentals_listing (
-            title, 
-            date_added, 
-            city, 
-            price, 
-            bedrooms, 
-            bathrooms, 
-            sqft, 
-            url
-        ) 
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s);""", 
-        (
-            rental.get('title'),
-            datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-            rental.get('city'),
-            rental.get('price'),
-            rental.get('bedrooms'),
-            rental.get('bathrooms'),
-            rental.get('sqft'),
-            rental.get('url')
-        ))
+    # check if listing is already in the database
+    rental_url = rental.get('url')
+    cur.execute(f"SELECT url FROM nc_rentals_listing WHERE url = '{rental_url}'")
+    db_entry = cur.fetchone()
+    
+    # if listing is not in the database, insert it into database
+    if not db_entry:
+        cur.execute("""
+            INSERT INTO nc_rentals_listing (
+                title, 
+                date_added, 
+                city, 
+                price, 
+                bedrooms, 
+                bathrooms, 
+                sqft, 
+                url
+            ) 
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s);""", 
+            (
+                rental.get('title'),
+                datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                rental.get('city'),
+                rental.get('price'),
+                rental.get('bedrooms'),
+                rental.get('bathrooms'),
+                rental.get('sqft'),
+                rental.get('url')
+            ))
+
+        new_listing_count += 1
 
 conn.commit()
 conn.close()
+
+if new_listing_count == 1:
+    print(f'{new_listing_count} new listing added to the database.\n')
+else:
+    print(f'{new_listing_count} new listings added to the database.\n')
