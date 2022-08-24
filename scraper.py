c438c1fe20fa3dfa58f071b0d7682e311701fc59
Nicholas Salvatore <nmsalvatore@gmail.com>
2022-08-23 18:18:22 -0700

    Removed collins_data list of rentals

--- scraper.py
+++ scraper.py
@@ -8,7 +8,7 @@ from selenium import webdriver
 print('\nChecking for new listings...\n')
 
 paul_law_data = get_paul_law_data()
-collins_data = get_collins_data()
+# collins_data = get_collins_data()
 
 # get data from websites requiring selenium
 driver = webdriver.Firefox()
@@ -18,5 +18,6 @@ grass_roots_data = get_grass_roots_data(driver)
 driver.close()
 
 def get_all_rentals():
-    rentals = paul_law_data + collins_data + pam_data + acm_data + grass_roots_data
+    rentals = paul_law_data + pam_data + acm_data + grass_roots_data
+    # rentals = paul_law_data
     return rentals
