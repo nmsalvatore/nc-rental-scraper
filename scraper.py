0eb85f1608ad7b7cf6b26c68007ae54914bf46c2
Nicholas Salvatore <nmsalvatore@gmail.com>
2022-08-24 06:48:31 -0700

    Implemented rental list concatenation with itertools chain function

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+import itertools
 from paullaw import get_paul_law_data
 from collins import get_collins_data
 from pam import get_pam_data
@@ -7,17 +8,24 @@ from selenium import webdriver
 
 print('\nChecking for new listings...\n')
 
+# get data using requests module
 paul_law_data = get_paul_law_data()
-# collins_data = get_collins_data()
 
-# get data from websites requiring selenium
+# get data using selenium module
 driver = webdriver.Firefox()
+collins_data = get_collins_data(driver)
 pam_data = get_pam_data(driver)
 acm_data = get_acm_data(driver)
 grass_roots_data = get_grass_roots_data(driver)
 driver.close()
 
 def get_all_rentals():
-    rentals = paul_law_data + pam_data + acm_data + grass_roots_data
-    # rentals = paul_law_data
-    return rentals
+    all_rental_data = [
+        paul_law_data,
+        collins_data,
+        pam_data,
+        acm_data,
+        grass_roots_data
+    ]
+
+    return list(itertools.chain(*all_rental_data))
