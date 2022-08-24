cdd624de0897d3fcfddbc0ddc2a354d821384697
Nicholas Salvatore <nmsalvatore@gmail.com>
2022-08-24 06:43:50 -0700

    Modified collins.py to use selenium instead of requests-html

--- collins.py
+++ collins.py
@@ -1,52 +1,62 @@
 import re
-from requests_html import HTMLSession
+import time
+from bs4 import BeautifulSoup
 from utils import get_int
 
-def get_collins_data():
+def get_collins_data(driver):
     # render Collins Property Management rental list
-    session = HTMLSession()
-    r = session.get('https://www.collinspropertymanagement.net/grass-valley-homes-for-rent')
-    r.html.render()
+    root_url = 'https://www.collinspropertymanagement.net'
+    driver.get(root_url + '/grass-valley-homes-for-rent')
+    time.sleep(2)
+    soup = BeautifulSoup(driver.page_source, 'lxml')
 
     rentals = []
 
-    listings = r.html.find('.listing-container .view-details')
+    listings = soup.find_all(class_='listing-container')
     for listing in listings:
         # get url
-        url = list(listing.absolute_links)[0]
-
-        # render page
-        details = session.get(url)
-        details.html.render()
-        html = details.html
-
+        href = listing.select('.view-details a.frs-btn')[0].get('href')
+        url = root_url + href
+        
         # get title
-        title = html.find('span.street-address', first=True).text
+        title = listing.find(class_='address').getText()
 
-        # get city
-        city_regex = re.compile(r'\n(.*),')
-        full_address = html.find('div.prop-address', first=True).text
-        city = city_regex.search(full_address).group(1)
+        # get city groups with regex
+        full_city = listing.find(class_='city').getText()
+        city_regex = re.compile(r'(.*),(.*)\s(.*)')
+        city_groups = re.search(city_regex, full_city)
+        city = city_groups.group(1).strip()
 
-        # get list price
-        price_str = html.find('div.prop-rent.text-alt', first=True).text
-        price = get_int(price_str)
+        # get price
+        price_str = listing.find(class_='rentAmount').getText()
+        price_regex = re.compile(r'(\d+),*(\d+)')
+        price_groups = re.search(price_regex, price_str).groups()
+        price = int(''.join(price_groups))
 
         # get bedrooms
-        bed_str = html.find('div.prop-beds', first=True).text
-        bedrooms = get_int(bed_str)
+        info = listing.find(class_='info').getText()
+        bed_regex = re.compile(r'(.*)Bed')
+        bedrooms = re.search(bed_regex, info).group(1).strip()
+        try:
+            bedrooms = int(bedrooms)
+        except:
+            bedrooms = float(bedrooms)
 
         # get bathrooms
-        bath_regex = re.compile(r'\s*(.*)\sba')
-        bath_str = html.find('div.prop-baths', first=True).text
+        bath_regex = re.compile(r'\s(\d.*)Bath')
+        bathrooms = re.search(bath_regex, info).group(1).strip()
         try:
-            bathrooms = int(bath_regex.search(bath_str).group(1))
+            bathrooms = int(bathrooms)
         except:
-            bathrooms = float(bath_regex.search(bath_str).group(1))
+            bathrooms = float(bathrooms)
 
         # get square footage
-        sqft_str = html.find('div.prop-area', first=True).text
-        sqft = get_int(sqft_str)
+        sqft_str = listing.find(class_='sqft').getText()
+        sqft_num_str = ''.join(re.findall(r'\d+', sqft_str))
+        try:
+            sqft = int(sqft_num_str)
+        except:
+            sqft = None
 
         listing_data = {
             'title': title,
