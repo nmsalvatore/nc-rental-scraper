dba042c78aef7bf327dd8ae629db5b3d34440dba
Nicholas Salvatore <nmsalvatore@gmail.com>
2022-08-27 07:46:56 -0700

    Created function for retrieval of each listing field

--- collins.py
+++ collins.py
@@ -1,73 +1,90 @@
 import re
 import time
 from bs4 import BeautifulSoup
-from utils import get_int
 
-def get_collins_data(driver):
-    # render Collins Property Management rental list
-    root_url = 'https://www.collinspropertymanagement.net'
-    driver.get(root_url + '/grass-valley-homes-for-rent')
+
+def get_rentals(driver):
+    # get source code and parse document as xml
+    root_path = 'https://www.collinspropertymanagement.net'
+    driver.get(root_path + '/grass-valley-homes-for-rent')
     time.sleep(2)
     soup = BeautifulSoup(driver.page_source, 'lxml')
 
+    # initialize rental list
     rentals = []
 
-    listings = soup.find_all(class_='listing-container')
+    # get elements for all rental listings
+    listing_class_name = 'listing-container'
+    listings = soup.find_all(class_=listing_class_name)
+
+    # get data from each rental listing
     for listing in listings:
-        # get url
-        href = listing.select('.view-details a.frs-btn')[0].get('href')
-        url = root_url + href
-        
-        # get title
-        title = listing.find(class_='address').getText()
-
-        # get city groups with regex
-        full_city = listing.find(class_='city').getText()
-        city_regex = re.compile(r'(.*),(.*)\s(.*)')
-        city_groups = re.search(city_regex, full_city)
-        city = city_groups.group(1).strip()
-
-        # get price
-        price_str = listing.find(class_='rentAmount').getText()
-        price_regex = re.compile(r'(\d+),*(\d+)')
-        price_groups = re.search(price_regex, price_str).groups()
-        price = int(''.join(price_groups))
-
-        # get bedrooms
-        info = listing.find(class_='info').getText()
-        bed_regex = re.compile(r'(.*)Bed')
-        bedrooms = re.search(bed_regex, info).group(1).strip()
-        try:
-            bedrooms = int(bedrooms)
-        except:
-            bedrooms = float(bedrooms)
-
-        # get bathrooms
-        bath_regex = re.compile(r'\s(\d.*)Bath')
-        bathrooms = re.search(bath_regex, info).group(1).strip()
-        try:
-            bathrooms = int(bathrooms)
-        except:
-            bathrooms = float(bathrooms)
-
-        # get square footage
-        sqft_str = listing.find(class_='sqft').getText()
-        sqft_num_str = ''.join(re.findall(r'\d+', sqft_str))
-        try:
-            sqft = int(sqft_num_str)
-        except:
-            sqft = None
-
         listing_data = {
-            'title': title,
-            'city': city,
-            'price': price,
-            'bedrooms': bedrooms,
-            'bathrooms': bathrooms,
-            'sqft': sqft,
-            'url': url,
+            'title': get_title(listing),
+            'city': get_city(listing),
+            'price': get_price(listing),
+            'bedrooms': get_bedrooms(listing),
+            'bathrooms': get_bathrooms(listing),
+            'sqft': get_sqft(listing),
+            'url': get_url(listing, root_path),
         }
 
-        rentals.append(listing_data)
-    
+        rentals.append(listing_data)    
+
     return rentals
+
+
+def get_url(listing, root_path):
+    selectors = '.view-details a.frs-btn';
+    relative_path = listing.select(selectors)[0].get('href')
+    absolute_path = root_path + relative_path
+    return absolute_path
+
+
+def get_title(listing):
+    title_el = listing.find(class_='address')
+    title = title_el.getText()
+    return title
+
+
+def get_city(listing):
+    full_city_el = listing.find(class_='city')
+    full_city_str = full_city_el.getText()
+    city_regex = re.compile(r'^(.*),\s*[A-Z]{2}')
+    city_match = re.search(city_regex, full_city_str)
+    city = city_match.group(1).strip()
+    return city
+
+
+def get_price(listing):
+    price_el = listing.find(class_='rentAmount')
+    price_str = price_el.getText()
+    price_regex = re.compile(r'(\d+),*(\d+)')
+    price_groups = re.search(price_regex, price_str).groups()
+    price = int(''.join(price_groups))
+    return price
+
+
+def get_bedrooms(listing):
+    info_el = listing.find(class_='info')
+    info_str = info_el.getText()
+    bed_regex = re.compile(r'(\d.*)Bed')
+    bed_match = re.search(bed_regex, info_str)
+    bedrooms = bed_match.group(1).strip()
+    return bedrooms
+
+
+def get_bathrooms(listing):
+    info_el = listing.find(class_='info')
+    info_str = info_el.getText()
+    bath_regex = re.compile(r'\s(\d.*)Bath')
+    bath_match = re.search(bath_regex, info_str)
+    bathrooms = bath_match.group(1).strip()
+    return bathrooms
+
+
+def get_sqft(listing):
+    sqft_el = listing.find(class_='sqft')
+    sqft_str = sqft_el.getText()
+    sqft = ''.join(re.findall(r'\d+', sqft_str))
+    return sqft
