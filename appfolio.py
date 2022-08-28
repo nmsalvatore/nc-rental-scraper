2b6c60d947d0ce82bbdbf51cdbb16468cd6a6fcd
Nicholas Salvatore <nmsalvatore@gmail.com>
2022-08-28 15:14:35 -0700

    Created module for appfolio scraping and implemented into acm.py, grassroots.py, pam.py

---
+++ appfolio.py
@@ -0,0 +1,98 @@
+import re
+import time
+from bs4 import BeautifulSoup
+
+def get_rentals(driver, root_path, listing_path):
+    # get source code and parse document as xml
+    driver.get(root_path + listing_path)
+    time.sleep(2)
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+
+    # initialize rental list
+    rentals = []
+
+    # get elements for all rental listings
+    listings = soup.find_all(class_='listing-item')
+    
+    print(f'Scanning {len(listings)} listings on {root_path}/')
+
+    # get data from each rental listing and add to rental list
+    for listing in listings:
+        listing_data = {
+            'title': get_title(listing),
+            'city': get_city(listing),
+            'price': get_price(listing),
+            'bedrooms': get_bedrooms(listing),
+            'bathrooms': get_bathrooms(listing),
+            'sqft': get_sqft(listing),
+            'url': get_url(listing, root_path),
+        }
+        rentals.append(listing_data)
+    
+    return rentals
+
+
+def get_url(listing, root_path):
+    relative_url_path = listing.find(class_='slider-link').get('href')
+    return root_path + relative_url_path
+
+
+def get_title(listing):
+    try:
+        address = listing.find(class_='slider-link').getText()
+        regex = re.compile(r'(.*),.*,.*')
+        title = re.search(regex, address).group(1).strip()
+        return title
+    except:
+        return None
+
+
+def get_city(listing):
+    try:
+        address = listing.find(class_='slider-link').getText()
+        regex = re.compile(r'.*,(.*),.*')
+        city = re.search(regex, address).group(1).strip()
+        return city
+    except:
+        return None
+
+
+def get_price(listing):
+    try:
+        price_element = listing.find(class_='rent')
+        price_text = price_element.getText().split('.')[0]
+        price = int(''.join(re.findall(r'\d+', price_text)))
+        return price
+    except:
+        return None
+
+
+def get_bedrooms(listing):
+    try:
+        bed_element = listing.find(class_='beds')
+        bed_text = bed_element.getText()
+        regex = re.compile(r'^(.*)beds')
+        return re.search(regex, bed_text).group(1).strip()
+    except:
+        return None
+
+
+def get_bathrooms(listing):
+    try:
+        bath_element = listing.find(class_='baths')
+        bath_text = bath_element.getText()
+        regex = re.compile(r'^(.*)bath')
+        return re.search(regex, bath_text).group(1).strip()
+    except:
+        return None
+
+
+def get_sqft(listing):
+    try:
+        sqft_element = listing.find(class_='sqft')
+        sqft_text = sqft_element.getText()
+        regex = re.compile(r'\d+')
+        sqft = ''.join(re.findall(regex, sqft_text))
+        return sqft
+    except:
+        return None
--- grassroots.py
+++ grassroots.py
@@ -1,72 +1,7 @@
-import re
-import time
-from bs4 import BeautifulSoup
-from utils import get_int
+import appfolio
 
-def get_grass_roots_data(driver):
-    # Open Property Associates Management Co website in Firefox
-    root_url = 'https://www.grassrootspm.com'
-    driver.get(root_url + '/available-rentals')
-
-    # Wait 2 seconds for page to fully load
-    time.sleep(2)
-
-    soup = BeautifulSoup(driver.page_source, 'lxml')
-
-    rentals = []
-
-    listings = soup.find_all(class_='listing-item')
-    for listing in listings:
-        # get url
-        url = root_url + listing.find(class_='slider-link').get('href')
-        
-        # get address groups with regex
-        address = listing.find(class_='slider-link').getText()
-        address_regex = re.compile(r'(.*),(.*),(.*)')
-        address_groups = re.search(address_regex, address)
-
-        # get title
-        title = address_groups.group(1).strip()
-
-        # get city
-        city = address_groups.group(2).strip()
-
-        # get price
-        price_str = listing.find(class_='rent').getText()
-        price = get_int(price_str)
-
-        # get bedrooms
-        bedrooms_el = listing.find(class_='beds')
-        if bedrooms_el is not None:
-            bedrooms = get_int(bedrooms_el.getText())
-
-        # get bathrooms
-        bath_regex = re.compile(r'^\s*(.*)\sbath')
-        bath_el = listing.find(class_='baths')
-        if bath_el is not None:
-            bath_str = re.search(bath_regex, bath_el.getText())
-            try:
-                bathrooms = int(bath_str.group(1))
-            except:
-                bathrooms = float(bath_str.group(1))
-        
-        # get square footage
-        sqft_el = listing.find(class_='sqft')
-        if sqft_el is not None:
-            sqft = get_int(sqft_el.getText())
-        else:
-            sqft = None
-
-        listing_data = {
-            'title': title,
-            'city': city,
-            'price': price,
-            'bedrooms': bedrooms,
-            'bathrooms': bathrooms,
-            'sqft': sqft,
-            'url': url,
-        }
-
-        rentals.append(listing_data)
-
+def get_rentals(driver):
+    root_path = 'https://www.grassrootspm.com'
+    listing_path = '/available-rentals'
+    rentals = appfolio.get_rentals(driver, root_path, listing_path)
     return rentals
