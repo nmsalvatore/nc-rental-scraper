abc0935b1b2e256e364d1b1366b13f2b07737a90
Nicholas Salvatore <nmsalvatore@gmail.com>
2022-09-01 07:24:20 -0700

    Modified hourly scheduling to :00 every hour

--- scheduler.py
+++ scheduler.py
@@ -2,7 +2,7 @@ import time
 import schedule
 import db_update
 
-schedule.every().hour.do(db_update.update)
+schedule.every().hour.at(':00').do(db_update.update)
 while True:
     schedule.run_pending()
     time.sleep(1)
