e4f0a64eaab1b5f7a8cab20c0ccb8e01ab803b00
Nicholas Salvatore <nmsalvatore@gmail.com>
2022-09-04 08:01:24 -0700

    Added note of when updates will occur at initialization of scheduler

--- scheduler.py
+++ scheduler.py
@@ -2,6 +2,7 @@ import time
 import schedule
 import db_update
 
+print('\nNOTE: Updates will occur every hour on the hour.\n')
 schedule.every().hour.at(':00').do(db_update.update)
 while True:
     schedule.run_pending()
