5e0d60ca2473c31e3193ef79b17a5de2a3e0735c
Nicholas Salvatore <nmsalvatore@gmail.com>
2022-09-08 07:49:55 -0700

    Fixed return of '0' rather than None

--- paullaw.py
+++ paullaw.py
@@ -2,12 +2,11 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
+
 def get_rentals():
     root_path = 'https://paullawpropertymanagement.managebuilding.com'
     res = requests.get(root_path + '/Resident/public/rentals')
     
-    # check for listings until listings are found
-    # TODO: check if there are no actual listings
     listings = None
     while not listings:
         soup = BeautifulSoup(res.text, 'lxml')
@@ -29,10 +28,12 @@ def get_rentals():
         rentals.append(listing_data)
     return rentals
 
+
 def get_url(listing, root):
     relative_path = listing.get('href')
     absolute_path = root + relative_path
     return absolute_path
+
 
 def get_title(listing):
     title_class_name = 'featured-listing__title'
@@ -40,34 +41,36 @@ def get_title(listing):
     title = title_el.getText()
     return title
 
+
 def get_city(listing):
     city_regex = re.compile(r'^(.*),')
     city_match = re.search(city_regex, listing.get('data-location'))
     city = city_match.group(1)
     return city
 
+
 def get_price(listing):
     price_str = listing.get('data-rent')
     price = int(price_str)
     return price
 
+
 def get_bedrooms(listing):
     bedrooms = listing.get('data-bedrooms')
-    try:
-        bedrooms = int(bedrooms)
-    except:
-        bedrooms = float(bedrooms)
-    return bedrooms or None
+    if bedrooms == '0':
+        return None
+    return bedrooms
+
 
 def get_bathrooms(listing):
     bathrooms = listing.get('data-bathrooms')
-    try:
-        bathrooms = int(bathrooms)
-    except:
-        bathrooms = float(bathrooms)
-    return bathrooms or None
+    if bathrooms == '0':
+        return None
+    return bathrooms
+
 
 def get_square_feet(listing):
-    sqft_str = listing.get('data-square-feet')
-    sqft = int(sqft_str)
-    return sqft or None
+    sqft = listing.get('data-square-feet')
+    if sqft == '0':
+        return None
+    return sqft
